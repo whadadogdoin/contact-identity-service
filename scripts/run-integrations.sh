57f62b62c8288afb0a2802039cb7849c9dc4ad24
Abhishek <abhishek.vk396@gmail.com>
2025-06-05 05:23:15 +0530

    run-integration script update

--- scripts/run-integrations.sh
+++ scripts/run-integrations.sh
@@ -1,6 +1,7 @@
-docker-compose up -d
+docker compose up -d
 echo '🟡 - Waiting for database to be ready...'
-./scripts/wait-for-it.sh "postgresql://postgres:mysecretpassword@localhost:5432/postgres" -- echo '🟢 - Database is ready!'
+chmod +x ./scripts/wait-for-it.sh
+./scripts/wait-for-it.sh "localhost:5432" -- echo '🟢 - Database is ready!'
 npx prisma migrate dev --name init
 npm run test
-docker-compose down
+docker compose down
