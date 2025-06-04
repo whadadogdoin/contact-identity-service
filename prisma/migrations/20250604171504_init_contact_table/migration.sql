44d0457d1f7552463f7af8074d8179b68c00163d
Abhishek <abhishek.vk396@gmail.com>
2025-06-04 22:47:23 +0530

    Bootstraped prisma and express

---
+++ prisma/migrations/20250604171504_init_contact_table/migration.sql
@@ -0,0 +1,16 @@
+-- CreateEnum
+CREATE TYPE "linkPrecedenceType" AS ENUM ('primary', 'secondary');
+
+-- CreateTable
+CREATE TABLE "Contact" (
+    "id" SERIAL NOT NULL,
+    "phoneNumber" TEXT,
+    "email" TEXT,
+    "linkedId" INTEGER,
+    "linkPrecedence" "linkPrecedenceType" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Contact_pkey" PRIMARY KEY ("id")
+);
