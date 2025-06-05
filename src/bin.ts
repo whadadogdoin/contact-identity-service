ccb7ddc41f4622a891e1757062d5aa37a8d79585
Abhishek <abhishek.vk396@gmail.com>
2025-06-05 05:41:29 +0530

    Modify .env to add PORT

--- src/bin.ts
+++ src/bin.ts
@@ -1,5 +1,7 @@
 import { app } from "./index";
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log("Server is running on port ",PORT);
 })
